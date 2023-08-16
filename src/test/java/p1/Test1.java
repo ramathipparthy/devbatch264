fdb2c6a844b0773894af67375e5b5da6fcc321db
deviacpt@gmail.com <deviacpt@gmail.com>
2023-08-16 14:16:54 +0530

    this is my first commit

---
+++ src/test/java/p1/Test1.java
@@ -0,0 +1,57 @@
+package p1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test1 
+{
+RemoteWebDriver driver;
+@Test(priority=1)
+public void setUp()
+{
+	//open browser
+	WebDriverManager.chromedriver().setup();
+	driver=new ChromeDriver();
+	//maximize
+	driver.manage().window().maximize();
+	
+}
+@Test(priority=2)
+public void openUrl() 
+{
+	//launch site
+	driver.get("http://www.google.com");
+	if(driver.getCurrentUrl().contains("http"))
+	{
+		Assert.assertTrue(true);
+	}
+	else
+	{
+		Assert.assertTrue(false);//stops execution
+	}
+	
+}
+/*@Test(priority=3)
+public void getTitle() 
+{
+Reporter.log("site is opened");
+
+	SoftAssert sa=new SoftAssert();
+	if(driver.getTitle().equals("Google"))
+	{
+		sa.assertTrue(true);
+	}
+	else
+	{
+		sa.assertTrue(false);
+	}
+	//driver.close();
+	sa.assertAll();
+}*/
+}
